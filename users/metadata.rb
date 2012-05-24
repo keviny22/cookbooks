0e537c076050d6050085a01de02c50cbba4122ab
Brett Weaver <brett@weav.net>
2012-05-24 11:05:18 -0700

    updating metadata

--- users/metadata.rb
+++ users/metadata.rb
@@ -1,9 +1,9 @@
 maintainer       "Opscode, Inc."
 maintainer_email "cookbooks@opscode.com"
 license          "Apache 2.0"
-description      "Creates Users From Users Local Databag"
+description      "Creates Users From Local Databag"
 version          "1.0.1"
-recipe "users::sysadmin", "searches users data bag for sysadmins and creates users"
+recipe "users::default", "adds users from data bag"
 
 %w{ redhat }.each do |os|
   supports os
