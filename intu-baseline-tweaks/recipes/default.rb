f516e84a4e538375f22ed7b0b9e7bfdffabf0d2b
Brett Weaver <brett@weav.net>
2012-05-25 08:20:29 -0700

    updated default to use new recipe

--- intu-baseline-tweaks/recipes/default.rb
+++ intu-baseline-tweaks/recipes/default.rb
@@ -1,3 +1,3 @@
 include_recipe "intu-baseline-tweaks::umask"
 include_recipe "intu-baseline-tweaks::disable_password_expiration"
-include_recipe "intu-baseline-tweaks::logrotate"
+include_recipe "intu-baseline-tweaks::chef_logrotate"
