01ea7f094c2b5afd213f033b73a528ffdae4a0ed
Brett Weaver <brett@weav.net>
2012-06-19 11:07:23 -0700

    adding oracle java

--- java/attributes/default.rb
+++ java/attributes/default.rb
@@ -18,7 +18,17 @@
 # limitations under the License.
 
 # default jdk attributes
-default['java']['install_flavor'] = "openjdk"
+default['java']['install_flavor'] = "oracle"
 default['java']['jdk_version'] = '6'
 default['java']['arch'] = kernel['machine'] =~ /x86_64/ ? "x86_64" : "i586"
 default['java']['java_home'] = "/usr/lib/jvm/java"
+
+# jdk6 attributes
+# x86_64
+default['java']['jdk']['6']['x86_64']['url'] = 'https://s3-us-west-1.amazonaws.com/intu-artifacts-us-west-1/rhel6/jdk-6u25-linux-x64.bin'
+default['java']['jdk']['6']['x86_64']['checksum'] = '43eabe42bb8d6e8dfe33f3a815f265ac5d5bb586'
+
+# jdk7 attributes
+# x86_64
+default['java']['jdk']['7']['x86_64']['url'] = 'http://download.example.com/otn-pub/java/jdk/7u2-b13/jdk-7u2-linux-x64.tar.gz'
+default['java']['jdk']['7']['x86_64']['checksum'] = '411a204122c5e45876d6edae1a031b718c01e6175833740b406e8aafc37bc82d'
