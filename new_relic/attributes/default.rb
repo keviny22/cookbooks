80cf625024b6578707479a522a6e7c2863c3e80e
Tommy Bishop <bishop.thomas@gmail.com>
2012-06-18 21:55:07 -0700

    add support for configureing parameter capture

--- new_relic/attributes/default.rb
+++ new_relic/attributes/default.rb
@@ -1,7 +1,8 @@
 default['new_relic']['config']['apdex_t']        = '0.5'
 default['new_relic']['config']['app_name']       = 'change_me'
-default['new_relic']['config']['file_path']      = 'change_me'
+default['new_relic']['config']['capture_params'] = false
 default['new_relic']['config']['developer_mode'] = false
+default['new_relic']['config']['file_path']      = 'change_me'
 default['new_relic']['config']['license_key']    = 'change_me'
 default['new_relic']['config']['log_level']      = 'info'
 default['new_relic']['config']['monitor_mode']   = true
