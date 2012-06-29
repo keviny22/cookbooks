2c7e9369bd6b92344e2353e9d7bce8a62d8ffc88
Tommy Bishop <bishop.thomas@gmail.com>
2012-06-29 10:05:06 -0700

    bump new relic cookbook version and update changelog

--- new_relic/metadata.rb
+++ new_relic/metadata.rb
@@ -2,7 +2,7 @@ maintainer       "Intuit Inc"
 maintainer_email "thomas_bishop@intuit.com"
 license          "All rights reserved"
 description      "New Relic"
-version          "0.1.0"
+version          "0.2.0"
 
 %w{ redhat }.each do |os|
   supports os
