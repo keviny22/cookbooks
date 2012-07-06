6d8c2c829fb634930137abb8e44ef0bdaaacdf70
Brett Weaver <brett@weav.net>
2012-07-06 08:51:06 -0700

    moving simple deploy to lc repo

--- intu-simple-deploy/attributes/default.rb
+++
@@ -1,2 +0,0 @@
-default["simple_deploy"]["gem_binary"] = '/usr/bin/gem'
-default["simple_deploy"]["version"] = '0.2.7'
--- intu-simple-deploy/metadata.rb
+++
@@ -1,8 +0,0 @@
-maintainer       "Intuit, Inc."
-maintainer_email "brett_weaver@intuit.com"
-description      "Installs Simple Deploy Gem"
-version          "0.1.0"
-
-%w{ redhat }.each do |os|
-  supports os
-end
