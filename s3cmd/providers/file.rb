b3c8555ee0ce822e9e7a9ef1b969dbae8f0aeade
Brett Weaver <brett@weav.net>
2012-08-08 14:53:50 -0700

    removing leading / if present

--- s3cmd/providers/file.rb
+++ s3cmd/providers/file.rb
@@ -3,10 +3,11 @@ action :download do
   file_name = new_resource.file_name
   bucket = new_resource.bucket
   object_name = new_resource.object_name
+  object_name.slice!(0) if object_name.start_with '/'
   force = new_resource.force ? '--force' : ''
 
   execute "Downloading #{file_name} from s3" do
-    command "s3cmd #{force} get s3://#{bucket}#{object_name} #{file_name}"
+    command "s3cmd #{force} get s3://#{bucket}/#{object_name} #{file_name}"
   end
 
   file file_name do
