1125b80fa161dac72795829833b6ca6019dbc523
Brett Weaver <brett@weav.net>
2012-08-16 13:52:26 -0700

    fixing domain

--- chef_handler/files/default/handlers/campfire.rb
+++ chef_handler/files/default/handlers/campfire.rb
@@ -12,7 +12,7 @@ class Chef
       def report
         base_cmd = "curl -k -u #{@token}:X -H 'Content-Type: application/json' -d"
         message = "{'message':{'body':'#{run_status.formatted_exception}'}}"
-        url = "https://#{@subdomain}/room/#{@room}/speak.json"
+        url = "https://#{@subdomain}.campfirenow.com/room/#{@room}/speak.json"
         cmd = "#{base_cmd} #{message} #{url}" 
         `#{cmd}`
       end
