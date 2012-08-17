56c48e234b46930f4802358cd320422360ad44ef
Brett Weaver <brett@weav.net>
2012-08-17 05:12:32 -0700

    updated formating

--- chef_handler/files/default/handlers/campfire.rb
+++ chef_handler/files/default/handlers/campfire.rb
@@ -4,16 +4,16 @@ class Chef
 
       def initialize(subdomain, token, room, message)
         @subdomain = subdomain
-        @token = token
-        @room = room
-        @message = message
+        @token     = token
+        @room      = room
+        @message   = message
       end
 
       def report
-        base_cmd = "curl -k -u #{@token}:X -H 'Content-Type: application/json' -d"
-        body = " \"{'message':{'body':'#{@message} #{run_status.formatted_exception}'}}\" "
-        url = "https://#{@subdomain}.campfirenow.com/room/#{@room}/speak.json"
-        cmd = "#{base_cmd} #{body} #{url}" 
+        base_cmd = "curl -s -m 60 -k -u #{@token}:X -H 'Content-Type: application/json'"
+        body     = "-d \"{'message':{'body':'#{@message} #{run_status.formatted_exception}'}}\" "
+        url      = "https://#{@subdomain}.campfirenow.com/room/#{@room}/speak.json"
+        cmd      = "#{base_cmd} #{body} #{url}" 
         `#{cmd}`
       end
 
