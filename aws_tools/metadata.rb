a211679b69ef7c8ca423aa033e066b0a45466ba1
Brett Weaver <brett@weav.net>
2012-08-30 10:17:07 -0700

    renamed aws to aws tools, aws_opscode to aws

---
+++ aws_tools/metadata.rb
@@ -0,0 +1,10 @@
+maintainer       "Intuit Inc"
+maintainer_email "brett_weaver@intuit.com"
+license          "All rights reserved"
+description      "AWS Cookbook"
+version          "0.1.0"
+depends          "python"
+
+%w{ redhat }.each do |os|
+  supports os
+end
