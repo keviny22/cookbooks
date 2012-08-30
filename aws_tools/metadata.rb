691c3d662710835c94c2e3948505185e1705cc23
Brett Weaver <brett@weav.net>
2012-08-30 13:04:24 -0700

    removes depend

--- aws_tools/metadata.rb
+++ aws_tools/metadata.rb
@@ -3,7 +3,6 @@ maintainer_email "brett_weaver@intuit.com"
 license          "All rights reserved"
 description      "AWS Cookbook"
 version          "0.1.0"
-depends          "python"
 
 %w{ redhat }.each do |os|
   supports os
