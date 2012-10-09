d3747cecd34367fa16badfa2c1f3d53f902b63c1
Brett Weaver <brett@weav.net>
2012-10-09 10:50:15 -0700

    updated version

--- sqlite/attributes/default.rb
+++ sqlite/attributes/default.rb
@@ -1,2 +1,2 @@
-default["sqlite"]["version"]       = '3.6.20-3.6.20'
-default["sqlite-devel"]["version"] = '3.6.20-3.6.20'
+default["sqlite"]["version"]       = '3.6.20-1.el6'
+default["sqlite-devel"]["version"] = '3.6.20-1.el6'
