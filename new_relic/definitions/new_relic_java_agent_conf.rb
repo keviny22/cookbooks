3916adff0c68e632b938a82709a1d9a10ec5ac86
Tommy Bishop <bishop.thomas@gmail.com>
2012-10-17 20:51:22 -0700

    fix typo in new relic java agent config definition

--- new_relic/definitions/new_relic_java_agent_conf.rb
+++ new_relic/definitions/new_relic_java_agent_conf.rb
@@ -1,5 +1,5 @@
 define :new_relic_java_agent_config do
-  template params['name'] do
+  template params[:name] do
     backup false
     cookbook 'new_relic'
     source 'java_agent_newrelic.yml.erb'
