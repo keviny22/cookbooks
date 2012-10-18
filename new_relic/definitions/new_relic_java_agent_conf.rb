11da16662c0b2d4ce51343ab047f73e6a33423fd
Tommy Bishop <bishop.thomas@gmail.com>
2012-10-17 21:05:35 -0700

    fix issue with new relic java agent definition

--- new_relic/definitions/new_relic_java_agent_conf.rb
+++ new_relic/definitions/new_relic_java_agent_conf.rb
@@ -1,5 +1,7 @@
 define :new_relic_java_agent_config do
-  template params[:name] do
+  node['new_relic']['config']['file_path'] = params[:name]
+
+  template node['new_relic']['config']['file_path'] do
     backup false
     cookbook 'new_relic'
     source 'java_agent_newrelic.yml.erb'
