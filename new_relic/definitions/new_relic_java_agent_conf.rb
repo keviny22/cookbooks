65b378bd74a7ca24bd8fe075a84aaa281e6836e5
Tommy Bishop <bishop.thomas@gmail.com>
2012-10-17 18:16:39 -0700

    remove new relic jar file and update java defintion as it should be in the artifact build

--- new_relic/definitions/new_relic_java_agent_conf.rb
+++ new_relic/definitions/new_relic_java_agent_conf.rb
@@ -1,16 +1,5 @@
 define :new_relic_java_agent_config do
-  node['new_relic']['config']['file_path'] = params[:name]
-
-  jar_path = File.join File.dirname(node['new_relic']['config']['file_path']),
-                       'newrelic.jar'
-
-  cookbook_file jar_path do
-    backup false
-    cookbook 'new_relic'
-    source 'newrelic.jar'
-  end
-
-  template node['new_relic']['config']['file_path'] do
+  template params['name'] do
     backup false
     cookbook 'new_relic'
     source 'java_agent_newrelic.yml.erb'
