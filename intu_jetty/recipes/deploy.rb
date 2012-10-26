11896ccd7467120117ab7d8a9a59f6209f81f96d
Rick Mendes <rmendes@intuit.com>
2012-10-25 17:26:07 -0700

    now creates the logs dir needed at runtime

--- intu_jetty/recipes/deploy.rb
+++ intu_jetty/recipes/deploy.rb
@@ -5,6 +5,14 @@ user "jetty" do
   not_if { File.exists? "/home/jetty" }
 end
 
+directory "/home/jetty/logs" do
+  owner "jetty"
+  group "jetty"
+  mode "0755"
+  action :create
+  not_if { File.exists? "/home/jetty/logs" }
+end
+
 yum_package "jetty-hightide-server" do
   action :install
   not_if { File.exists? node['intu']['jetty']['home'] }
@@ -12,7 +20,7 @@ end
 
 template "/etc/default/jetty" do
   source "jetty-config-options.erb"
-  mode 0644
+  mode "0644"
   owner node['intu']['jetty']['user']
   group node['intu']['jetty']['group']
 end
