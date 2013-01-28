9dc5774367ffd1f8ac9be86f80b183e76ae139b4
Rick Mendes <rmendes@intuit.com>
2013-01-28 10:12:22 -0800

    jetty now stores logs on the ephemeral disk

--- intu_jetty/recipes/default.rb
+++ intu_jetty/recipes/default.rb
@@ -1,3 +1,7 @@
+class Chef::Recipe
+  include Ec2DeviceHelpers
+end
+
 jetty_user_home = node['intu']['jetty']['user_home']
 user = node['intu']['jetty']['user']
 group = node['intu']['jetty']['group']
@@ -8,10 +12,35 @@ user "#{user}" do
   shell "/bin/bash"
 end
 
-directory "#{jetty_user_home}/logs" do
+log_dir = File.join local_device_directory, node['intu']['jetty']['log_dir']
+
+directory log_dir do
   owner user
   group group
-  mode "0755"
+  mode '0755'
+  recursive true
+end
+
+ruby_block "move #{jetty_user_home}/logs/* to #{log_dir}" do
+  block do
+    FileUtils.mv Dir.glob("#{jetty_user_home}/logs/*"), log_dir
+  end
+  not_if do
+    File.symlink? "#{jetty_user_home}/logs"
+  end
+end
+
+directory "#{jetty_user_home}/logs" do
+  action :delete
+  not_if do
+    File.symlink? "#{jetty_user_home}/logs"
+  end
+end
+
+link "#{jetty_user_home}/logs" do
+  owner user
+  group group
+  to log_dir
 end
 
 package "jetty-hightide-server" do
